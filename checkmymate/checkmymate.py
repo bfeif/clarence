45dd4b7395ebdb7b69333990b515b79c744b358e
Ben Feifke <benfeifke@gmail.com>
2022-01-14 22:14:32 +0100

    Added logging; enriched styling; enriched routing.

--- checkmymate/checkmymate.py
+++ checkmymate/checkmymate.py
@@ -1,18 +1,23 @@
+import logging
 from flask import Blueprint, Flask, render_template, request, redirect, url_for
 from . import utils
+import logging
+app = Flask(__name__)
+logger = app.logger
+logger.setLevel(logging.DEBUG)
 bp = Blueprint('checkmymate', __name__)
 
 @bp.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        lichess_name = request.form['lichess_name']
-        return redirect(url_for('data', lichess_name=lichess_name))
     return render_template("index.html")
 
 @bp.route('/data/', methods=['POST'])
 def data():
     lichess_name = request.form.get("lichess_name")
-    opening_stats_struct = utils.get_lichess_user_opening_stats(lichess_name)
+    num_games = request.form.get("num_games")
+    if not utils.is_user(lichess_name):
+        return redirect("/")
+    opening_stats_struct = utils.get_lichess_user_opening_stats(lichess_name, num_games)
     return render_template('data.html',
                            lichess_name=lichess_name,
                            opening_stats_struct=opening_stats_struct)
--- checkmymate/utils.py
+++ checkmymate/utils.py
@@ -60,15 +60,16 @@ def is_legal_game(game_dict):
         return True
     return False
 
-def get_lichess_user_opening_stats(lichess_username):
+def get_lichess_user_opening_stats(lichess_username, num_games):
     """
     Wrapper function for getting all the formatted openings of a lichess user.
     """
+    print(num_games)
     query = {
         "opening": True,
         "moves": False,
         "sort": "dateDesc",
-        "max": 10}
+        "max": num_games}
     games = [process_game_dict(g, lichess_username)
             for g in lichess.api.user_games(lichess_username, **query)
             if is_legal_game(g)]
@@ -82,7 +83,7 @@ def get_lichess_user_opening_stats(lichess_username):
                 ["points"]
                 .agg(["count", "mean"])
                 .sort_values("count", ascending=False)
-                .head(10)
+                .head(5)
                 .reset_index()
                 .rename({"opening_name_simple": "opening",
                          "count": "num_games",
@@ -92,9 +93,19 @@ def get_lichess_user_opening_stats(lichess_username):
         for color in ["white", "black"]}
     return opening_stats
 
+def is_user(lichess_username):
+    """
+    Wrapper function to check if a user exists.
+    """
+    try:
+        user = lichess.api.user(lichess_username)
+        return True
+    except lichess.api.ApiHttpError:
+        return False
+
 # main
 if __name__=="__main__":
 
     # load the games
     user = 'normanrookwell'
-    print(get_lichess_user_opening_stats(user))
+    print(get_lichess_user_opening_stats(user, 10))
